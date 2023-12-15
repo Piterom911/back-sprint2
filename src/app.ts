324f65c6fb617a093afc7253de424e9e5c2e79d2
Roman Dogoda <piterom911@gmail.com>
2023-12-15 17:40:14 +0100

    add auth middleware to delete method

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import {videosRouter} from "./routes/videos-router";
 import {db} from "./db/db";
 import {blogsRouter} from "./routes/blogs-router";
 import {postsRouter} from "./routes/posts-router";
+import {authMiddleware} from "./middlewares/auth/auth-middleware";
 export const app = express()
 
 app.use(express.json())
@@ -10,7 +11,7 @@ app.use('/videos', videosRouter)
 app.use('/blogs', blogsRouter)
 app.use('/posts', postsRouter)
 
-app.delete('/testing/all-data', (req: Request, res: Response) => {
+app.delete('/testing/all-data', authMiddleware, (req: Request, res: Response) => {
     db.videos = []
     db.posts = []
     db.blogs = []
