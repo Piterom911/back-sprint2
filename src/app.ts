510d3de50202029c5a55d731b63691a7843018f3
Roman Dogoda <piterom911@gmail.com>
2023-12-15 19:59:55 +0100

    fix bag with delete

--- src/app.ts
+++ src/app.ts
@@ -6,17 +6,18 @@ import {postsRouter} from "./routes/posts-router";
 import {authMiddleware} from "./middlewares/auth/auth-middleware";
 export const app = express()
 
+app.delete('/testing/all-data', (req: Request, res: Response) => {
+    db.videos.length = 0
+    db.posts.length = 0
+    db.blogs.length = 0
+    res.sendStatus(204)
+})
+
 app.use(express.json())
 app.use('/videos', videosRouter)
 app.use('/blogs', blogsRouter)
 app.use('/posts', postsRouter)
 
-app.delete('/testing/all-data', authMiddleware, (req: Request, res: Response) => {
-    db.videos = []
-    db.posts = []
-    db.blogs = []
-    res.sendStatus(204)
-})
 
 
 
