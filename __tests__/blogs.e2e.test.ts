5c71dbae6a141d7a67d6bb0b62fa0f0a2c6f5a57
Roman Dogoda <piterom911@gmail.com>
2023-12-17 14:48:20 +0100

    Create blogs tests

--- __tests__/blogs.e2e.test.ts
+++ __tests__/blogs.e2e.test.ts
@@ -1,107 +1,115 @@
-// import request from 'supertest'
-// import {VideoCreateModel, VideoUpdateModel} from "../src/models/videos/input";
-// import {app} from "../src/app";
-//
-// const getRequest = () => request(app)
-//
-// describe('Check "/blogs" endpoint', () => {
-//     beforeAll(async () => {
-//         await request(app).delete('/testing/all-data')
-//     })
-//
-//     it('should return status-code 200 and an empty array', async () => {
-//         await request(app)
-//             .get('/blogs')
-//             .expect(200, [])
-//     })
-//
-//     it('should return status 404 for not existing blogs', async () => {
-//         await request(app)
-//             .get('/blogs/938475')
-//             .expect(404)
-//     })
-//
-//     it('should`nt create an object with incorrect video properties', async () => {
-//         await request(app)
-//             .post('/videos')
-//             .send({title: 'An attempt'})
-//             .expect(400)
-//
-//         await request(app)
-//             .get('/videos')
-//             .expect(200, [])
-//     })
-//
-//     let videoExampleForTests: any = undefined;
-//     it('should  create an object with correct video properties', async () => {
-//         const newVideoReqData: VideoCreateModel = {
-//             title: 'Ein Versuch',
-//             author: 'Das bin ich',
-//             availableResolutions: ['P144']
-//         }
-//
-//         const createdVideoObj = {
-//             ...newVideoReqData,
-//             id: expect.any(Number),
-//             canBeDownloaded: false,
-//             minAgeRestriction: null,
-//             createdAt: expect.any(String),
-//             publicationDate: expect.any(String)
-//         }
-//
-//         videoExampleForTests = await request(app)
-//             .post('/videos')
-//             .send(newVideoReqData)
-//             .expect(201)   // !!!!!!!!!!!!!!!!!!!!!!!!!!!!  .expect(201, createdVideoObj)
-//
-//         expect(videoExampleForTests.body).toEqual(createdVideoObj)
-//
-//         const responseAllVideos = await request(app)
-//             .get('/videos')
-//             .expect(200) // !!!!!!!!!!!!!!!!!!!!!!!!!!!!  ..expect(200, [createdVideoObj])
-//
-//         expect(responseAllVideos.body).toEqual([createdVideoObj])
-//     })
-//
-//     it('should update video data', async () => {
-//         const updateVideoReqData: VideoUpdateModel = {
-//             title: 'Put new data to this video object',
-//             author: 'Of course me',
-//             availableResolutions: ['P144'],
-//             canBeDownloaded: true,
-//             minAgeRestriction: null,
-//             publicationDate: new Date().toISOString()
-//         }
-//
-//         await request(app)
-//             .put('/videos/' + videoExampleForTests.body.id)
-//             .send(updateVideoReqData)
-//             .expect(204)
-//     })
-//
-//     it('should`nt update video data', async () => {
-//         const updateVideoReqData = {
-//             author: 'Of course me',
-//         }
-//
-//         const response = await request(app)
-//             .put('/videos/' + videoExampleForTests.body.id)
-//             .send(updateVideoReqData)
-//             .expect(400)
-//
-//         expect(response.body).toEqual({
-//             errorsMessages: expect.arrayContaining([{ message: expect.any(String), field: expect.any(String) }])
-//         })
-//     })
-//
-//     it('deletes target video object', async () => {
-//         videoExampleForTests = await request(app)
-//             .delete('/videos/' + videoExampleForTests.body.id )
-//             .expect(204)
-//
-//         await request(app)
-//             .get('/videos')
-//             .expect(200, [])
-//     })
-//
-// })
+import request from 'supertest'
+import {app} from "../src/app";
+import {URI_PATHS} from "../src/models/common";
+import any = jasmine.any;
+
+const getRequest = () => request(app)
+
+describe('Endpoints videos', () => {
+    let token: any
+
+    beforeAll(async () => {
+        await getRequest().delete(URI_PATHS.tests)
+    })
+
+    it('should return status 200 and an empty array', async () => {
+        await getRequest()
+            .get(URI_PATHS.blogs)
+            .expect(200, [])
+    })
+
+    it('should return status 404 for not existing blog', async () => {
+        await getRequest()
+            .get(`${URI_PATHS.blogs}/938475`)
+            .expect(404)
+    })
+
+    it('should return 401 status code', async () => {
+        await getRequest()
+            .post(URI_PATHS.blogs)
+            .set('Authorization', `Basic YWRtaW46cXdlcnR5asd`)
+            .send({name: 'An attempt'})
+            .expect(401)
+
+        await getRequest()
+            .get(URI_PATHS.blogs)
+            .expect(200, [])
+    })
+
+    it('should`nt create an object with incorrect blog properties', async () => {
+        await getRequest()
+            .post(URI_PATHS.blogs)
+            .set('Authorization', `Basic YWRtaW46cXdlcnR5`)
+            .send({name: 'An attempt'})
+            .expect(400)
+
+        await getRequest()
+            .get(URI_PATHS.blogs)
+            .expect(200, [])
+    })
+
+    let blogExampleForTests: any = undefined;
+    it('should  create an object with correct video properties', async () => {
+        const newBlogReqData = {
+            name: 'Ein Versuch',
+            description: 'Das bin ich',
+            websiteUrl: 'https://ZzcQfsPbtTQEQYkCBJogfcQRdWGrh-2vIArtzFwlWbLg7hzm215YimA3LtvxwUdYiB.M4ruVPXLhKE2gQAZM1mShLlLE'
+        }
+
+        blogExampleForTests = await getRequest()
+            .post(URI_PATHS.blogs)
+            .set('Authorization', `Basic YWRtaW46cXdlcnR5`)
+            .send(newBlogReqData)
+            .expect(201)
+
+        expect(blogExampleForTests.body).toEqual({...newBlogReqData, id: expect.any(String)})
+
+        const responseAllBlogs = await getRequest()
+            .get(URI_PATHS.blogs)
+            .expect(200) // !!!!!!!!!!!!!!!!!!!!!!!!!!!!  ..expect(200, [createdVideoObj])
+
+        expect(responseAllBlogs.body).toEqual([blogExampleForTests.body])
+    })
+
+    it('should update video data', async () => {
+        const updateBlogReqData = {
+            name: 'Ein',
+            description: 'Das',
+            websiteUrl: 'https://www.shortVeryShort'
+        }
+
+        await request(app)
+            .put(`${URI_PATHS.blogs}/` + blogExampleForTests.body.id)
+            .set('Authorization', `Basic YWRtaW46cXdlcnR5`)
+            .send(updateBlogReqData)
+            .expect(204)
+    })
+
+    it('should`nt update video data', async () => {
+        const updateBlogReqData = {
+            websiteUrl: 'https://shortVeryShort'
+        }
+
+        const response = await getRequest()
+            .put(`${URI_PATHS.blogs}/` + blogExampleForTests.body.id)
+            .set('Authorization', `Basic YWRtaW46cXdlcnR5`)
+            .send(updateBlogReqData)
+            .expect(400)
+
+        expect(response.body).toEqual({
+            errorsMessages: expect.arrayContaining([{ message: expect.any(String), field: expect.any(String) }])
+        })
+    })
+
+    it('deletes target video object', async () => {
+        blogExampleForTests = await getRequest()
+            .delete(`${URI_PATHS.blogs}/` + blogExampleForTests.body.id )
+            .set('Authorization', `Basic YWRtaW46cXdlcnR5`)
+            .expect(204)
+
+        await request(app)
+            .get(URI_PATHS.blogs)
+            .expect(200, [])
+    })
+
+})
